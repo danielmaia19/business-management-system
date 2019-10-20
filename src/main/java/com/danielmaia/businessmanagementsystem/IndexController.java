25619f3906bdc4e856fc2c89dfb30b9ef2636e31
Dan Maia <dannymaia1992@gmail.com>
2019-10-20 16:23:09 +0100

    Spring boot & Spring MVC Initialised Project

---
+++ src/main/java/com/danielmaia/businessmanagementsystem/IndexController.java
@@ -0,0 +1,18 @@
+package com.danielmaia.businessmanagementsystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public void greeting() {
+
+    }
+
+}
