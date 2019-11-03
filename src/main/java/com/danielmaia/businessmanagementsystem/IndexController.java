d6242a0155850758d3674dc15a9e65e9a60a59e5
Daniel Maia <dannymaia1992@gmail.com>
2019-11-03 13:55:11 +0000

    Updated master

--- src/main/java/com/danielmaia/businessmanagementsystem/IndexController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/IndexController.java
@@ -11,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/index")
-    public void greeting() {
+    public void index() {
+
+    }
+
+    @GetMapping("/test")
+    public void test() {
 
     }
 
