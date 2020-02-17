792b30d66d374356edb5373208c8a7c991852c63
Daniel Maia <dannymaia1992@gmail.com>
2020-02-17 19:00:01 +0000

    Created a requesting mapping to the root of the website to then redirect users to the login page. This is temporary until a front-end page has been created int the future

--- src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
@@ -1,8 +1,6 @@
 package com.danielmaia.businessmanagementsystem.Controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import com.danielmaia.businessmanagementsystem.HelperClasses.AuthenticationSystem;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +9,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
-
-    @GetMapping("/login")
+    // This is the root page.
+    // At the moment the homepage gets redirected to the login page
+    @GetMapping("/")
     public String index() {
-        return "login";
+        return "redirect:/login";
     }
 
-    // When the form is submitted this will be actioned.
+
+    // The login page will be where users login.
+    // It first checks if the user is already logged in.
+    // If they are then they are redirected to their dashboard
+    // If not then to the login screen.
+    @GetMapping("login")
+    public String login() {
+        if (!AuthenticationSystem.isLogged()) return "login"; // or some logic
+        return "redirect:/dashboard";
+    }
+
+
+
+    // This will check if the user is already logged in.
+    // If they are, then they get redirected to the '/dashboard' page.
+    // Otherwise they will be redirected to the 'login'
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String loginAnother() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth.getPrincipal() instanceof UserDetails) {
-            return "redirect:/dashboard";
-        } else return "login";
+    public String logUserIn() {
+        if (!AuthenticationSystem.isLogged())  return "redirect:/dashboard";
+        return "login";
     }
 
 
