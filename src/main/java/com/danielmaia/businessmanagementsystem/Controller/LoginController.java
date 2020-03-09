d7ccf79995aba2f86e1fe623af1f184f5e03e75c
Daniel Maia <dannymaia1992@gmail.com>
2020-03-09 22:16:34 +0000

    Fixed: Dashboard page after signup
    Fixed the dashboard page load at signup and now working and added current users name on screen.

--- src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
@@ -1,15 +1,7 @@
 package com.danielmaia.businessmanagementsystem.Controller;
 
-import com.danielmaia.businessmanagementsystem.HelperClasses.AuthenticationSystem;
-import com.danielmaia.businessmanagementsystem.Model.User;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.security.Principal;
 
 @Controller
