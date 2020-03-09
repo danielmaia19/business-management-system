fb9e0142725cf154ddf6f2309821c0071b0215f7
Daniel Maia <dannymaia1992@gmail.com>
2020-03-09 20:33:59 +0000

    Cannot not access login after success login
    User is no longer able to go to the login screen after they have successfully logged in.

--- src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
@@ -1,43 +1,23 @@
 package com.danielmaia.businessmanagementsystem.Controller;
 
 import com.danielmaia.businessmanagementsystem.HelperClasses.AuthenticationSystem;
+import com.danielmaia.businessmanagementsystem.Model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+
 @Controller
 public class LoginController {
 
-    /** This is the root page.
-     * At the moment the homepage gets redirected to the login page
-     * This prevents the user from going back to the login page if the
-     * user is already logged in.
-     */
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/login";
-    }
-
-    /** The login page will be where users login.
-     * It first checks if the user is already logged in.
-     * If they are then they are redirected to their dashboard
-     * If not then to the login screen.
-     */
-    @GetMapping("login")
-    public String login() {
-        if (!AuthenticationSystem.isLogged()) return "login"; // or some logic
-        return "redirect:/dashboard";
-    }
-
-    /** This will check if the user is already logged in.
-     * If they are, then they get redirected to the '/dashboard' page.
-     * Otherwise they will be redirected to the 'login'
-     */
-    @RequestMapping(value = "login",method = RequestMethod.POST)
-    public String logUserIn() {
-        if (!AuthenticationSystem.isLogged())  return "redirect:/dashboard";
-        return "login";
+    @RequestMapping(value = "/login")
+    public String index(Principal principal) {
+        return principal == null ?  "login" : "redirect:/";
     }
 
 }
