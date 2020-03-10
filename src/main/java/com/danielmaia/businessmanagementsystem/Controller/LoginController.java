cad7abcacf1686b85e16c0c35bafc03aa24e4a5f
Daniel Maia <dannymaia1992@gmail.com>
2020-03-10 19:03:16 +0000

    Replaced login redirect:/ with forward:/

--- src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
@@ -1,15 +1,19 @@
 package com.danielmaia.businessmanagementsystem.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
 import java.security.Principal;
 
 @Controller
 public class LoginController {
 
     @RequestMapping({"/", "/login"})
-    public String index(Principal principal) {
-        return principal == null ?  "login" : "/dashboard";
+    public String index(Principal principal, ModelMap model) {
+        model.addAttribute("view", "dashboard");
+        return principal == null ?  "login" : "forward:/dashboard";
     }
 
 }
