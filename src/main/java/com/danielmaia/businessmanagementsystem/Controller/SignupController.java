395c00c53951b84ae6d6982aea8bf458f39d7c6a
Daniel Maia <dannymaia1992@gmail.com>
2020-03-20 23:35:26 +0000

    Altered role repository to use role service

--- src/main/java/com/danielmaia/businessmanagementsystem/Controller/SignupController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Controller/SignupController.java
@@ -1,33 +1,21 @@
 package com.danielmaia.businessmanagementsystem.Controller;
 
-import com.danielmaia.businessmanagementsystem.Model.Role;
 import com.danielmaia.businessmanagementsystem.Repository.RoleRepository;
-import com.danielmaia.businessmanagementsystem.Repository.UserRepository;
 import com.danielmaia.businessmanagementsystem.Model.User;
 import com.danielmaia.businessmanagementsystem.Service.RoleService;
 import com.danielmaia.businessmanagementsystem.Service.UserService;
-import org.omg.CORBA.Object;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.management.remote.JMXAuthenticator;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.Arrays;
 
 @Controller
 public class SignupController {
@@ -37,6 +25,9 @@ public class SignupController {
 
     @Autowired
     private RoleRepository roleRepository;
+
+    @Autowired
+    private RoleService roleService;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -69,7 +60,7 @@ public class SignupController {
             String password = user.getPassword();
             user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
             user.setEnabled(true);
-            user.setRole(roleRepository.findByName("ROLE_ADMIN"));
+            user.setRole(roleService.findByName("ROLE_ADMIN"));
             userService.saveUser(user);
 
             try {
