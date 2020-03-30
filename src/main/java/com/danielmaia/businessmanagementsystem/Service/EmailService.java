d6c8c86f8cab796e4dbdb5328f74046abdb74df6
Daniel Maia <dannymaia1992@gmail.com>
2020-03-31 00:13:48 +0100

    Added link to the password reset email

--- src/main/java/com/danielmaia/businessmanagementsystem/Service/EmailService.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Service/EmailService.java
@@ -4,7 +4,11 @@ import com.danielmaia.businessmanagementsystem.Model.Mail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -21,6 +25,19 @@ public class EmailService {
         message.setFrom(mail.getFrom());
 
         emailSender.send(message);
+    }
+
+    public void sendHtmlMessage(Mail mail) throws MessagingException {
+
+        MimeMessage mailMessage = emailSender.createMimeMessage();
+        mailMessage.setSubject(mail.getSubject(), "UTF-8");
+        MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true, "UTF-8");
+        helper.setFrom(mail.getFrom());
+        helper.setTo(mail.getTo());
+        helper.setText(mail.getContent(), true);
+
+        emailSender.send(mailMessage);
+
     }
 
 }
