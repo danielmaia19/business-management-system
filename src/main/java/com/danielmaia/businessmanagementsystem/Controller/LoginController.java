c60b24c68e2804dafae8db15b9f6ddd17a1c2d8a
Daniel Maia <dannymaia1992@gmail.com>
2020-04-15 16:07:22 +0100

    Added the tracking of the number of active users

--- src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
+++ src/main/java/com/danielmaia/businessmanagementsystem/Controller/LoginController.java
@@ -2,17 +2,23 @@ package com.danielmaia.businessmanagementsystem.Controller;
 
 import com.danielmaia.businessmanagementsystem.Model.Role;
 import com.danielmaia.businessmanagementsystem.Repository.RoleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Map;
 
 @Controller
+@Slf4j
 public class LoginController {
+
+    @Autowired
+    SessionRegistry sessionRegistry;
 
     @Autowired
     private RoleRepository roleRepository;
@@ -32,6 +38,20 @@ public class LoginController {
         if (auth.getPrincipal() instanceof UserDetails) {
             return "forward:/dashboard";
         } else return "login";
+    }
+
+    @RequestMapping(value = "/loggedUsers")
+    public String admin(Map<String, Object> model) {
+
+        if(sessionRegistry.getAllPrincipals().size() != 0) {
+            log.info("ACTIVE USER: " + sessionRegistry.getAllPrincipals().size());
+            model.put("activeuser",  sessionRegistry.getAllPrincipals().size());
+        }
+        else
+            log.warn("EMPTY" );
+
+        log.debug(" access ADMIN page. Access granted.");
+        return "loggedUsers";
     }
 
 }
